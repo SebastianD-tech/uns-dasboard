27254db8827d61bfbc4a609176b7ba3302323318
SebastianD-tech <sebastian.deinl@liebherr.com>
2025-06-11 19:47:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("Dashboard-Skript geladen.");
 // WICHTIG: Tragen Sie hier Ihre HiveMQ Cloud-Daten ein!
 const options = {
   host: 'e511a0ad851e4fdaafd32f113a294998.s1.eu.hivemq.cloud', // URL von Ihrer HiveMQ-Seite
-  port: 8884,                                  // WSS Port von Ihrer HiveMQ-Seite
+  port: 8883,                                  // WSS Port von Ihrer HiveMQ-Seite
   protocol: 'wss',                             // 'wss' steht für sichere WebSockets
   username: 'UNSTester2',           // Username aus dem Access Management
   password: 'UNSTester!33'      // Passwort aus dem Access Management
